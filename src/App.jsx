418cd13ce87248b78ca6ac1613472dfcef21854f
razip77 <razip@puo.edu.my>
2025-05-06 17:28:31 +0800

    Add files via upload
    Projek eBook LTspice

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>Modul eBook Simulasi LTspice</h1>
+      <p>Selamat datang ke modul interaktif. Sila pilih eksperimen untuk mula.</p>
+    </div>
+  );
+}
+export default App;
